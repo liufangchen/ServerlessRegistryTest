9d3a670cde525119190407657aefd541bb19230c
liufangchen <1171988327@qq.com>
2022-04-14 03:13:00 +0800

    修改preInit function

--- hook/index.js
+++ hook/index.js
@@ -1,5 +1,13 @@
 async function preInit(inputObj) {
-    console.log("初始化前")
+    console.log(`
+    _    _ ______ _    _ ______ 
+   | |  | |  ____| |  | |  ____|
+   | |__| | |__  | |__| | |__   
+   |  __  |  __| |  __  |  __|  
+   | |  | | |____| |  | | |____ 
+   |_|  |_|______|_|  |_|______|
+                                
+                                `)
 }
 
 async function postInit(inputObj) {
